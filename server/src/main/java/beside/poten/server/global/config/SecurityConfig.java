f5709a2834d056e0712700d444dce7f2639540a1
Sunro1994 <tjsfhdhkd@naver.com>
2024-05-19 03:45:37 +0900

    oauth2Login 문법 최신 버전에 맞게 수정, 이동된 AuthenticationManager (ApplicationConfig -> SecurityConfig), CustomJsonUsernamePassword Bean 추가

--- server/src/main/java/beside/poten/server/global/config/SecurityConfig.java
+++ server/src/main/java/beside/poten/server/global/config/SecurityConfig.java
@@ -1,8 +1,13 @@
 package beside.poten.server.global.config;
 
+import beside.poten.server.global.filter.CustomJsonUsernamePasswordAuthenticationFilter;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -10,11 +15,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
 
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final ObjectMapper objectMapper;
 
 
     @Bean
@@ -32,9 +40,30 @@ public class SecurityConfig {
                         authorizeRequests
                                 .requestMatchers("/api/v1/account/login").permitAll() // 로그인 허용
                                 .anyRequest().authenticated()
-                ).oauth2Login()
-                .defaultSuccessUrl("/login", true);
+                ).oauth2Login( login -> login.defaultSuccessUrl("/login", true));
+        http.addFilterAfter(customJsonUsernamePasswordAuthenticationFilter(), LogoutFilter.class);
+
 
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder());
+        return new ProviderManager(provider);
+    }
+
+
+
+    @Bean
+    public CustomJsonUsernamePasswordAuthenticationFilter customJsonUsernamePasswordAuthenticationFilter() {
+        CustomJsonUsernamePasswordAuthenticationFilter customJsonUsernamePasswordLoginFilter
+                = new CustomJsonUsernamePasswordAuthenticationFilter(objectMapper);
+        customJsonUsernamePasswordLoginFilter.setAuthenticationManager(authenticationManager());
+
+
+
+        return customJsonUsernamePasswordLoginFilter;
     }
 }
