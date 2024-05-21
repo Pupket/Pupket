34ba87916001602be50972a4fa7ed7ba2cc2c4a9
Sunro Lee <tjsfhdhkd@naver.com>
2024-05-21 11:21:45 +0900

    파일 업로드, 이미지 불러오기 구현

--- server/src/main/java/beside/poten/server/domain/user/controller/UserController.java
+++ server/src/main/java/beside/poten/server/domain/user/controller/UserController.java
@@ -8,22 +8,24 @@ import beside.poten.server.domain.user.service.FileService;
 import beside.poten.server.domain.user.service.UserService;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.amazonaws.util.IOUtils;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.coyote.BadRequestException;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.UrlResource;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
+
 
 @RestController
 @RequiredArgsConstructor
@@ -33,7 +35,6 @@ public class UserController {
 
     private final UserService userService;
     private final FileService fileService;
-    private final AmazonS3Client amazonS3Client;
     private final AmazonS3 amazonS3;
 
     @Value("${cloud.aws.s3.bucket}")
@@ -77,6 +78,7 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.OK).body(updatedUser);
     }
 
+
     @Schema(description = "회원 철회")
     @DeleteMapping("/delete/{id}")
     @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
@@ -88,6 +90,7 @@ public class UserController {
 
     }
 
+    //TODO:: 파일 유효성 검사, 저장할때 명명 규칙 정하기
     @Schema(description = "테스트용 파일 업로드")
     @PostMapping("/file-upload")
     public ResponseEntity fileDownload(@RequestParam("test")  MultipartFile multipartFile) throws IOException {
@@ -104,21 +107,19 @@ public class UserController {
 
     @Schema(description = "테스트용 파일 다운로드")
     @GetMapping("/file-download")
-    public ResponseEntity<UrlResource> downloadImage(String originalFilename) throws MalformedURLException {
+    public ResponseEntity<byte[]> getBucketFileUrl(String originalFilename, HttpServletRequest request) throws IOException {
+
+        String currentPath = request.getRequestURI();
+        log.info("currentPath ={}", currentPath);
+        S3Object s3Object = amazonS3.getObject(bucket, originalFilename);
 
 
-        // NCP Object Storage의 파일 URL을 생성
-        String fileUrl = "https://kr.object.ncloudstorage.com/" + bucket + "/" + originalFilename;
-        UrlResource urlResource = new UrlResource(fileUrl);
+        S3ObjectInputStream s3ObjectInputStream = s3Object.getObjectContent();
 
-        log.info("urlResource={}", urlResource);
-        String contentDisposition = "attachment; filename=\"" + originalFilename + "\"";
-        log.info("contentDisposition={}", contentDisposition);
+
 
         // header에 CONTENT_DISPOSITION 설정을 통해 클릭 시 다운로드 진행
-        return ResponseEntity.ok()
-                .header(HttpHeaders.CONTENT_DISPOSITION, contentDisposition)
-                .body(urlResource);
+        return ResponseEntity.status(HttpStatus.OK).contentType(MediaType.IMAGE_JPEG).body(IOUtils.toByteArray(s3ObjectInputStream));
 
 
     }
