2d60c89833f9a0fe6b660653bdffb0dca9690f5a
Sunro Lee <tjsfhdhkd@naver.com>
2024-05-21 15:42:29 +0900

    test Controller

---
+++ server/src/main/java/beside/poten/server/domain/matching/test.java
@@ -0,0 +1,15 @@
+package beside.poten.server.domain.matching;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class test {
+
+    @GetMapping("/test")
+    public String hello(){
+        return "hello";
+    }
+
+}
