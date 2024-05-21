383ecfcf40e049dfd57d6be3113cadf68d0c1f22
Sunro Lee <tjsfhdhkd@naver.com>
2024-05-21 19:58:24 +0900

    파일명 변환 기능, 사진 불러오기 추가, 유저 도메인에 적용

--- server/src/main/java/beside/poten/server/domain/user/controller/UserController.java
+++ server/src/main/java/beside/poten/server/domain/user/controller/UserController.java
@@ -1,30 +1,26 @@
 package beside.poten.server.domain.user.controller;
 
-import beside.poten.server.domain.user.dto.SignUpReq;
-import beside.poten.server.domain.user.dto.SocialSignUpReq;
-import beside.poten.server.domain.user.dto.UpdateReq;
+import beside.poten.server.domain.user.dto.request.SignUpReq;
+import beside.poten.server.domain.user.dto.request.SocialSignUpReq;
+import beside.poten.server.domain.user.dto.request.UpdateReq;
+import beside.poten.server.domain.user.dto.response.FindUserReq;
 import beside.poten.server.domain.user.entity.User;
 import beside.poten.server.domain.user.service.FileService;
 import beside.poten.server.domain.user.service.UserService;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.S3Object;
-import com.amazonaws.services.s3.model.S3ObjectInputStream;
-import com.amazonaws.util.IOUtils;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.coyote.BadRequestException;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @RestController
@@ -35,94 +31,90 @@ public class UserController {
 
     private final UserService userService;
     private final FileService fileService;
-    private final AmazonS3 amazonS3;
-
-    @Value("${cloud.aws.s3.bucket}")
-    private String bucket;
-
-
 
 
     @Schema(description = "일반회원가입")
     @PostMapping("/sign-up")
-    public ResponseEntity signUp(@RequestBody SignUpReq signUpReq) throws BadRequestException {
+    public ResponseEntity signUp(@RequestPart SignUpReq signUpReq, @RequestPart(value = "profileImg", required = false) MultipartFile multipartFile) throws IOException {
         log.info(signUpReq.getEmail());
 
-        userService.signUp(signUpReq);
+        userService.signUp(signUpReq, multipartFile);
 
 
         return ResponseEntity.status(HttpStatus.OK).build();
 
     }
 
+
     @Schema(description = "소셜 회원 추가정보 요청 ")
     @PostMapping("/social/sign-up")
     @PreAuthorize("hasRole('GUEST') ")
-    public ResponseEntity socialSignUp(@RequestBody SocialSignUpReq socialSignUpReq, HttpServletRequest request) {
+    public ResponseEntity socialSignUp(@RequestPart(value = "socialSignUpReq") SocialSignUpReq socialSignUpReq
+            , @RequestPart(value = "profileImg",required = false) MultipartFile profileImg
+            , HttpServletRequest request) throws IOException {
 
 
-        User socialMember = userService.signUpSocialMember(socialSignUpReq, request);
+        User socialMember = userService.signUpSocialMember(socialSignUpReq, request, profileImg);
 
 
-        return ResponseEntity.status(HttpStatus.OK).body(socialMember);
+        return ResponseEntity.ok(HttpStatus.OK);
+
+    }
+
+    @Schema(description = "토큰으로 회원 조회")
+    @GetMapping("/find-user")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+    public ResponseEntity findUser(HttpServletRequest request) throws IOException {
+
+        FindUserReq user = userService.findUser(request);
+        byte[] profileImg = userService.findPorfileImg(request);
+        List<Object> sendData = new ArrayList<>();
+        sendData.add(user);
+        sendData.add(profileImg);
+
+        return ResponseEntity.status(HttpStatus.OK).body(sendData);
+
+    }
+
+    @Schema(description = "id값으로 회원 조회")
+    @GetMapping("/find-user/{id}")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+    public ResponseEntity findUserById(@PathVariable("id") int id, HttpServletRequest request) throws IOException {
+
+
+        FindUserReq user = userService.findUser(id);
+        byte[] profileImg = userService.findPorfileImg(request);
+        List<Object> sendData = new ArrayList<>();
+        sendData.add(user);
+        sendData.add(profileImg);
+
+        return ResponseEntity.status(HttpStatus.OK).body(sendData);
 
     }
 
     @Schema(description = "회원 정보 수정")
     @PatchMapping("/update/{id}")
     @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
-    public ResponseEntity update(@PathVariable("id") Long id, @RequestBody UpdateReq updateReq) throws BadRequestException {
+    public ResponseEntity update(@PathVariable("id") int id, @RequestBody UpdateReq updateReq,@RequestPart(value = "profileImg", required = false) MultipartFile profileImg) throws IOException {
 
-        User updatedUser = userService.update(id, updateReq);
+        FindUserReq user = userService.update(id, updateReq, profileImg);
 
-        return ResponseEntity.status(HttpStatus.OK).body(updatedUser);
+        return ResponseEntity.status(HttpStatus.OK).body(user);
     }
 
 
     @Schema(description = "회원 철회")
     @DeleteMapping("/delete/{id}")
     @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
-    public ResponseEntity delete(@PathVariable("id") Long id) throws BadRequestException {
-
+    public ResponseEntity delete(@PathVariable("id") int id) throws BadRequestException {
+        FindUserReq user = userService.findUser((id));
+        fileService.deleteFile(user.getProfileImg());
         userService.delete(id);
 
         return ResponseEntity.status(HttpStatus.OK).build();
 
     }
 
-    //TODO:: 파일 유효성 검사, 저장할때 명명 규칙 정하기
-    @Schema(description = "테스트용 파일 업로드")
-    @PostMapping("/file-upload")
-    public ResponseEntity fileDownload(@RequestParam("test")  MultipartFile multipartFile) throws IOException {
-        if (multipartFile != null) {
-        fileService.saveFile(multipartFile);
-
-        }else{
-            log.info("multipartFile={}",multipartFile);
-        }
-
-
-        return ResponseEntity.status(HttpStatus.OK).build();
-    }
-
-    @Schema(description = "테스트용 파일 다운로드")
-    @GetMapping("/file-download")
-    public ResponseEntity<byte[]> getBucketFileUrl(String originalFilename, HttpServletRequest request) throws IOException {
-
-        String currentPath = request.getRequestURI();
-        log.info("currentPath ={}", currentPath);
-        S3Object s3Object = amazonS3.getObject(bucket, originalFilename);
-
-
-        S3ObjectInputStream s3ObjectInputStream = s3Object.getObjectContent();
-
-
-
-        // header에 CONTENT_DISPOSITION 설정을 통해 클릭 시 다운로드 진행
-        return ResponseEntity.status(HttpStatus.OK).contentType(MediaType.IMAGE_JPEG).body(IOUtils.toByteArray(s3ObjectInputStream));
-
-
-    }
 
 
 }
--- server/src/main/java/beside/poten/server/domain/user/service/UserService.java
+++ server/src/main/java/beside/poten/server/domain/user/service/UserService.java
@@ -3,9 +3,10 @@ package beside.poten.server.domain.user.service;
 import beside.poten.server.domain.user.constant.RoleType;
 import beside.poten.server.domain.user.constant.UserGender;
 import beside.poten.server.domain.user.constant.UserStatus;
-import beside.poten.server.domain.user.dto.SignUpReq;
-import beside.poten.server.domain.user.dto.SocialSignUpReq;
-import beside.poten.server.domain.user.dto.UpdateReq;
+import beside.poten.server.domain.user.dto.request.SignUpReq;
+import beside.poten.server.domain.user.dto.request.SocialSignUpReq;
+import beside.poten.server.domain.user.dto.request.UpdateReq;
+import beside.poten.server.domain.user.dto.response.FindUserReq;
 import beside.poten.server.domain.user.entity.User;
 import beside.poten.server.domain.user.repository.UserRepository;
 import beside.poten.server.global.jwt.service.JwtService;
@@ -19,7 +20,9 @@ import org.apache.coyote.BadRequestException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.Optional;
 
 import static beside.poten.server.domain.user.entity.QUser.*;
@@ -36,6 +39,7 @@ public class UserService {
 
 
     private final AmazonS3 amazonS3;
+    private final FileService fileService;
 
 
     @Value("${cloud.aws.s3.bucket}")
@@ -43,11 +47,17 @@ public class UserService {
 
 
 
-    public void signUp(SignUpReq signUpReq) throws BadRequestException {
+
+
+
+
+    public void signUp(SignUpReq signUpReq, MultipartFile profileImg) throws IOException {
+
 
         if (signUpReq.getEmail() == null || signUpReq.getPassword() == null) {
             throw new BadRequestException("아이디 또는 비밀번호가 비어 있습니다.");
         }
+
 
         User user = User.builder()
                 .email(signUpReq.getEmail())
@@ -61,6 +71,14 @@ public class UserService {
                 .gender(UserGender.valueOf(signUpReq.getGender()))
                 .build();
 
+        if (profileImg != null) {
+            String newName = fileService.convertName(profileImg.getOriginalFilename());
+            user.updateProfileImg(newName);
+            fileService.saveFile(profileImg, newName);
+        }
+
+
+
         user.encodePassword(passwordEncoder);
         user.updateCreateDate();
         user.updateLastModifed();
@@ -72,7 +90,9 @@ public class UserService {
 
     }
 
-    public User signUpSocialMember(SocialSignUpReq socialSignUpReq, HttpServletRequest request) {
+
+
+    public User signUpSocialMember(SocialSignUpReq socialSignUpReq, HttpServletRequest request, MultipartFile profileImg) throws IOException {
 
         String accessToken = jwtService.extractAccessToken(request);
         log.info("accessToken =" + accessToken);
@@ -80,21 +100,23 @@ public class UserService {
 
         log.info("loginId={}",loginId);
 
-
-
-
         User findMember = userRepository.findByEmail(loginId).get();
         log.info("memberOauthID={} ", findMember.getOauth2Id());
 
         findMember.updateKakaoMember(
                 socialSignUpReq.getName(),
                 socialSignUpReq.getNickname(),
-                socialSignUpReq.getProfileImage(),
                 socialSignUpReq.getBirthDay(),
                 socialSignUpReq.getContact(),
                 socialSignUpReq.getAddress(),
                 UserGender.valueOf(socialSignUpReq.getGender())
         );
+
+        if (profileImg != null) {
+            String newName = fileService.convertName(profileImg.getOriginalFilename());
+            findMember.updateProfileImg(newName);
+            fileService.saveFile(profileImg, newName);
+        }
         findMember.updateIsDeleted("N");
         findMember.updateCreateDate();
         findMember.updateLastModifed();
@@ -104,12 +126,12 @@ public class UserService {
 
     }
 
-    public User update(Long id, UpdateReq updateReq) throws BadRequestException {
-        Optional<User> findMemberById = userRepository.findById(id);
+    public FindUserReq update(int id, UpdateReq updateReq, MultipartFile profileImg) throws IOException {
+        Optional<User> findMemberById = userRepository.findById(Long.valueOf(id));
         if(findMemberById.isPresent()) {
-            User user = findMemberById.get();
+            User findMember = findMemberById.get();
 
-            user.updateMember(
+            findMember.updateMember(
                     updateReq.getName(),
                     updateReq.getNickname(),
                     updateReq.getProfileImage(),
@@ -117,9 +139,24 @@ public class UserService {
                     updateReq.getAddress()
             );
 
-            userRepository.save(user);
+            if (profileImg != null) {
+                String newName = fileService.convertName(profileImg.getOriginalFilename());
+                findMember.updateProfileImg(newName);
+                fileService.deleteFile(findMember.getProfileImage());
+                fileService.saveFile(profileImg, newName);
+            }
+            userRepository.save(findMember);
 
-            return user;
+            return FindUserReq.builder()
+                    .id(findMember.getId())
+                    .name(findMember.getName())
+                    .profileImg(findMember.getProfileImage())
+                    .contact(findMember.getContact())
+                    .address(findMember.getAddress())
+                    .nickname(findMember.getNickname())
+                    .build();
+
+
         }
         else{
             throw new BadRequestException();
@@ -128,16 +165,71 @@ public class UserService {
     }
 
     @Transactional
-    public void delete(Long id) throws BadRequestException {
+    public void delete(int id) throws BadRequestException {
 
-        Optional<User> findMemberById = userRepository.findById(id);
+        Optional<User> findMemberById = userRepository.findById(Long.valueOf(id));
         if(findMemberById.isPresent()) {
            jpaQueryFactory.update(user)
                    .set(user.status,UserStatus.WITHDRAW)
-                   .where(user.id.eq(id)).execute();
+                   .where(user.id.eq(Long.valueOf(id))).execute();
         }else {
             throw new BadRequestException();
         }
     }
 
+    public FindUserReq findUser(HttpServletRequest request) {
+        String accessToken = jwtService.extractAccessToken(request);
+        log.info("accessToken =" + accessToken);
+        String loginId=  jwtService.extractLoginIdFromAccessToken(accessToken);
+
+        log.info("loginId={}",loginId);
+
+        User findMember = userRepository.findByEmail(loginId).get();
+
+        return FindUserReq.builder()
+                .id(findMember.getId())
+                .name(findMember.getName())
+                .profileImg(findMember.getProfileImage())
+                .contact(findMember.getContact())
+                .address(findMember.getAddress())
+                .nickname(findMember.getNickname())
+                .gender(String.valueOf(findMember.getGender()))
+                .build();
+
+    }
+
+
+    public FindUserReq findUser(int id) {
+
+        log.info("id={}",id);
+        User findMember = userRepository.findById(Long.valueOf(id)).get();
+        log.info("findMember = {} " ,findMember);
+
+
+        return FindUserReq.builder()
+                .id(findMember.getId())
+                .name(findMember.getName())
+                .profileImg(findMember.getProfileImage())
+                .contact(findMember.getContact())
+                .address(findMember.getAddress())
+                .nickname(findMember.getNickname())
+                .build();
+
+    }
+
+    public byte[] findPorfileImg(HttpServletRequest request) throws IOException {
+
+        String accessToken = jwtService.extractAccessToken(request);
+        log.info("accessToken =" + accessToken);
+        String loginId=  jwtService.extractLoginIdFromAccessToken(accessToken);
+
+        log.info("loginId={}",loginId);
+
+        User findMember = userRepository.findByEmail(loginId).get();
+
+
+        return fileService.loadFileInS3(findMember.getProfileImage(), request);
+
+
+    }
 }
