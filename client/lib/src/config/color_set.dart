136ce661529fb46e1c451dd5839ea1c51cc93e1a
Metagauge Corp <metagaugecorp.@Metagaugeui-MacBookAir-2.local>
2024-05-22 22:59:17 +0900

    [feat] Add home UI

--- client/lib/src/config/color_set.dart
+++ client/lib/src/config/color_set.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
 class CustomColor {
-  static const Color backgroundColor = Color(0xFFE9E4DE);
-  // static const Color white1 = Color.fromARGB(255, 252, 254, 255);
+  static const Color lightBackgroundColor = Color(0xFFE9E4DE);
+  static const Color darkBackgroundColor = Color(0xFF4C433F);
+
+// static const Color white1 = Color.fromARGB(255, 252, 254, 255);
 
   // static const List<Color> gradient1 = [
   //   Color.fromARGB(255, 123, 99, 233),
